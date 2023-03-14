4d82217d8f5485af0be0656c583d8267d350cc14
Janniksf <janniksfredricsson@gmail.com>
2023-03-14 19:59:46 +0100

    All parameters are used in choice def

--- src/simulation.py
+++ src/simulation.py
@@ -2,13 +2,37 @@
 from trader import Trader, CEX, DEX, crypto_list
 
 # Define the variables for the simulation
-num_iterations = 10000
+num_iterations = 100
 
 # Define the trader strategy
 
 
 def choose_exchange(trader):
+    DEXoMeter = 0
+    CEXoMeter = 0
     if any(x in trader.crypto_currency for x in crypto_list[4:10]):
+        DEXoMeter += 1
+    else:
+        CEXoMeter += 1
+    if trader.fees_concern < 50:
+        DEXoMeter += 1
+    else:
+        CEXoMeter += 1
+    if trader.speed_concern < 50:
+        DEXoMeter += 1
+    else:
+        CEXoMeter += 1
+    if trader.security_concern < 50:
+        DEXoMeter += 1
+    else:
+        CEXoMeter += 1
+    if trader.regulation_concern == "low":
+        DEXoMeter += 1
+    elif trader.regulation_concern == "medium":
+        CEXoMeter += 1
+    else:
+        CEXoMeter += 1
+    if DEXoMeter > CEXoMeter:
         return "DEX"
     else:
         return "CEX"
@@ -17,17 +41,17 @@ def choose_exchange(trader):
 # Define the simulation function, where 1000 traders are created and they use choose_exchange to choose their exchange
 
 def run_simulation():
-    Dex = 0
-    Cex = 0
+    Dex = DEX()
+    Cex = CEX()
     for i in range(num_iterations):
-        trader = Trader()
+        trader = Trader(trader_id=i)
         exchange_choice = choose_exchange(trader)
         if exchange_choice == 'DEX':
-            Dex += 1
+            Dex.traders.append(trader)
         elif exchange_choice == 'CEX':
-            Cex += 1
-    print("DEX:", Dex)
-    print("CEX:", Cex)
+            Cex.traders.append(trader)
+    print("DEX:", len(Dex.traders))
+    print("CEX:", len(Cex.traders))
 
 
 run_simulation()
--- src/trader.py
+++ src/trader.py
@@ -6,12 +6,12 @@ crypto_list = ['BTC', 'ETH', 'XRP', 'BCH',
 
 
 class Trader:
-    def __init__(self):
-
+    def __init__(self, trader_id):
+        self.trader_id = trader_id
         self.crypto_currency = [random.choice(
-            crypto_list), random.choice(crypto_list)]
+            crypto_list)]
         # self.capital = random.randint(1000, 10000)
-        self.regulation_conceren = ["low", "medium", "high"]
+        self.regulation_concern = ["low", "medium", "high"]
         self.security_concern = random.randint(0, 100)
         self.speed_concern = random.randint(0, 100)
         self.fees_concern = random.randint(0, 100)
@@ -24,6 +24,7 @@ class CEX:
         self.speed = 75
         self.security = 25
         self.regulation = 90
+        self.traders = []
 
 
 class DEX:
@@ -33,3 +34,4 @@ class DEX:
         self.speed = 25
         self.security = 75
         self.regulation = 10
+        self.traders = []
