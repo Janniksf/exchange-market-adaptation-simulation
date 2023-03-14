c6091dd9d93949280514e7f2ee8560ff7876cfd7
Janniksf <janniksfredricsson@gmail.com>
2023-03-14 19:19:22 +0100

    Simple choose sim, traders choose by crypto pref

--- src/simulation.py
+++ src/simulation.py
@@ -1,1 +1,33 @@
-#First file
+
+from trader import Trader, CEX, DEX, crypto_list
+
+# Define the variables for the simulation
+num_iterations = 10000
+
+# Define the trader strategy
+
+
+def choose_exchange(trader):
+    if any(x in trader.crypto_currency for x in crypto_list[4:10]):
+        return "DEX"
+    else:
+        return "CEX"
+
+
+# Define the simulation function, where 1000 traders are created and they use choose_exchange to choose their exchange
+
+def run_simulation():
+    Dex = 0
+    Cex = 0
+    for i in range(num_iterations):
+        trader = Trader()
+        exchange_choice = choose_exchange(trader)
+        if exchange_choice == 'DEX':
+            Dex += 1
+        elif exchange_choice == 'CEX':
+            Cex += 1
+    print("DEX:", Dex)
+    print("CEX:", Cex)
+
+
+run_simulation()
---
+++ src/trader.py
@@ -0,0 +1,35 @@
+import random
+
+# create a list of top 10 cryptocurrencies
+crypto_list = ['BTC', 'ETH', 'XRP', 'BCH',
+               'LTC', 'EOS', 'XLM', 'ADA', 'TRX', 'XMR']
+
+
+class Trader:
+    def __init__(self):
+
+        self.crypto_currency = [random.choice(
+            crypto_list), random.choice(crypto_list)]
+        # self.capital = random.randint(1000, 10000)
+        self.regulation_conceren = ["low", "medium", "high"]
+        self.security_concern = random.randint(0, 100)
+        self.speed_concern = random.randint(0, 100)
+        self.fees_concern = random.randint(0, 100)
+
+
+class CEX:
+    def __init__(self):
+        self.crypto_currency = crypto_list[0:3]
+        self.fees = 25
+        self.speed = 75
+        self.security = 25
+        self.regulation = 90
+
+
+class DEX:
+    def __init__(self):
+        self.crypto_currency = crypto_list
+        self.fees = 75
+        self.speed = 25
+        self.security = 75
+        self.regulation = 10
